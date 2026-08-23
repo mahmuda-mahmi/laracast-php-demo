9a6ba3aa35e52236a3857c6872a0af6b59b13c6e
Mahmuda Akter <mahmudapakter@gmail.com>
2026-08-23 13:32:39 +0600

    practioced till array and functions

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <style>
+    <!-- <style>
         body {
             display: grid;
             height: 100vh;
@@ -13,9 +13,63 @@
             background-color: #eed8d8;
             margin: 0;
         }
-    </style>
+    </style> -->
 </head>
 <body>
+
+
+   <h1> Recommended Books</h1>
+
+    <?php
+    $books = [
+            [   
+                'name' => 'The Quotable Ronald Reagan',
+                'author' => 'By Reagan, Ronald and Hannaford, Peter',
+                'releaseYear' => 2001,
+                'purchaseUrl' => 'https://example.com/the-quotable-ronald-reagan'
+            ],
+            [   
+                'name' => 'Rooster crows for day',
+                'author' => 'By Burman, Ben Lucien',
+                'releaseYear' => 2005,
+                'purchaseUrl' => 'https://example.com/rooster-crows-for-day'
+            ],
+            [   
+                'name' => 'Leadership Is an Art',
+                'author' => 'By De Pree, Max',
+                'releaseYear' => 2010,
+                'purchaseUrl' => 'https://example.com/leadership-is-an-art'
+            ]
+        ];
+
+        function filterByAuthor($books, $author) {
+            $filteredBooks = [];
+            foreach ($books as $book) {
+                if( $book['author'] === $author) {
+                    $filteredBooks[] = $book;
+                }
+            }
+            return $filteredBooks;
+        }
+    ?>
+
+    
+
+    <ul>
+        <?php foreach(filterByAuthor($books, 'By De Pree, Max') as $book) : ?>
+            <li>
+                <?= $book['name'] ?>
+                <br>
+                <?= $book['releaseYear'] ?>
+                <br>
+                <?= $book['author'] ?>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+
+
+
+<!-- 
     <?php
     $name = "Dark Matter";
     $read = true;
@@ -29,7 +83,7 @@
     <h1>
         <?php echo $message; ?>
         <?= $message ?>
-    </h1>
+    </h1> -->
 
 </body>
-</html>
+ 
